52e63cd616e3d9418f0503d6e93aee40a633e0ea
kimberley <kim.dobney@yahoo.co.uk>
2021-11-09 12:27:02 +0000

    Set up files for test

---
+++ tests/Classes/MuppetDisplayTest.php
@@ -0,0 +1,11 @@
+<?php
+use \PHPUnit\Framework\TestCase;
+use Muppets\Classes\MuppetDisplay;
+
+class MuppetDisplayTest extends TestCase
+{
+public function testSuccess()
+{
+    $this->assertEquals(1, 1);
+}
+}
