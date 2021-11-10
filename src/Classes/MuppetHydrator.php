3f62ee5b78299f9549186b1f7f335fd602517be8
HannaSoderlund <hanna.ej.soderlund@gmail.com>
2021-11-10 10:24:20 +0000

    created getMuppetDetails public function in MuppetHydrator class

--- src/Classes/MuppetHydrator.php
+++ src/Classes/MuppetHydrator.php
@@ -17,4 +17,22 @@ class MuppetHydrator
         $query->execute();
         return $query->fetchAll();
     }
+
+    /**
+     * queries db to return details of selected muppet
+     *
+     * @param \PDO $db
+     * @param int $id
+     *
+     * @return array returns single muppet from db with the matching id from button
+     */
+    public static function getMuppetDetails(\PDO $db, int $id)
+    {
+
+        $query = $db->prepare("SELECT `id`, `name`, `debut_year`, `mayhem`, `glamour`, `humour`, `hall_of_fame`, `img_url` FROM `characters` WHERE `id` = :id");
+        $query->setFetchMode(\PDO::FETCH_CLASS, MuppetEntity::class);
+        $query->bindParam(':id', $id);
+        $query->execute();
+        return $query->fetchAll();
+    }
 }
