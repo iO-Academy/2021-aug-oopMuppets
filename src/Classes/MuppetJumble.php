82ac2ed9a3cf28b026ab0cec42aef5b782342ccb
mclack <miloclack@hotmail.co.uk>
2021-11-11 16:19:54 +0000

    Unit tests for getRandomId method complete and running successfully

--- src/Classes/MuppetJumble.php
+++ src/Classes/MuppetJumble.php
@@ -5,10 +5,15 @@ namespace Muppets\Classes;
 
 class MuppetJumble
 {
-    public static function getRandomId(array $muppets) : int
+    public static function getRandomId(array $muppets)
     {
         shuffle($muppets);
-        return $muppets[0]->getId();
+        $randomId = $muppets[0]->getId();
+        if ($randomId !== 0 && $randomId !== '') {
+            return $randomId;
+        } else {
+            return '404 Muppet not found - you\'s a muppet!';
+        }
     }
 
 }
